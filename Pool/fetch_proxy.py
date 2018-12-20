7e57212ad23fa98f817211d66c1aeb815e514cff
saber <saber@windows.com>
2018-12-20 20:05:02 +0800

    version 0.1.1 结构调整

--- Pool/fetch_proxy.py
+++ Pool/fetch_proxy.py
@@ -5,6 +5,8 @@ import random
 import time
 from bs4 import BeautifulSoup
 
+from Pool import verify_proxy
+
 usragent = ["Mozilla/5.0 (Windows NT 10.0; WOW64)", 'Mozilla/5.0 (Windows NT 6.3; WOW64)',
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
             'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
@@ -139,4 +141,5 @@ def getproxy_kuaidaili(url):
 if __name__ == "__main__":
     print("testing")
     myproxy.runspider()
-    myproxy.showresult()
+    # myproxy.showresult()
+    verify_proxy.getip(myproxy.proxyresult)
